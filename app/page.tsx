4e06f26699f767daf2d8633465a585313f9bc19b
Joonyeoup Kim <joonyeoupkim@Joonyeoups-MacBook-Air.local>
2025-02-09 23:56:26 -0500

    addition

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
       height: 200,
     },
     {
-      src: "/images/jupiterr.png",
+      src: "/images/jupiter.png",
       alt: "Real-Image Results",
       width: 800,
       height: 300,
